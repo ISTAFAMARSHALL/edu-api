022e0db534274b4e764a49f53d2581903fb9da3e
istafamarshall <istafa.a.marshall@gmail.com>
2023-05-30 12:50:55 -0400

    create teacher model

---
+++ db/migrate/20230530165042_create_teachers.rb
@@ -0,0 +1,15 @@
+class CreateTeachers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :teachers do |t|
+      t.string :name
+      t.string :address
+      t.string :subject
+      t.string :email
+      t.string :birthday
+      t.string :school_id
+      t.string :auth_level
+
+      t.timestamps
+    end
+  end
+end
