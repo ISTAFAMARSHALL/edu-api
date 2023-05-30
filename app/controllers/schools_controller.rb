7f1dd82ac8adc6dc7ff87ab1428126b3bd2813b4
istafamarshall <istafa.a.marshall@gmail.com>
2023-05-30 12:36:13 -0400

    add full CRUD to schools controller

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,2 +1,36 @@
 class SchoolsController < ApplicationController
+    
+    def index
+        schools = school.all
+        render json: schools, status: :ok
+    end
+    
+    def show
+        school = school.find(params[:id])
+        render json: school, status: :ok
+    end
+    
+    def create
+        school = school.create!(school_params)
+        render json: school, status: :created
+    end
+    
+    def update
+        school = school.find(params[:id])
+        updated_school = school.update!(school_params)
+        render json: school, status: :accepted
+    end
+    
+    def destroy
+        school = school.find(params[:id])
+        school.destroy
+        render json: school
+    end
+    
+    private
+    
+    def school_params
+        params.permit(:name, :address)
+    end
+
 end
