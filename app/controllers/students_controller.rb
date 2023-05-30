70c36888610fcad4ef1ae16a512cec8d359a3b7c
istafamarshall <istafa.a.marshall@gmail.com>
2023-05-30 12:37:26 -0400

    add full CRUD to teachers controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,36 @@
 class StudentsController < ApplicationController
+    
+    def index
+        students = student.all
+        render json: students, status: :ok
+    end
+    
+    def show
+        student = student.find(params[:id])
+        render json: student, status: :ok
+    end
+    
+    def create
+        student = student.create!(student_params)
+        render json: student, status: :created
+    end
+    
+    def update
+        student = student.find(params[:id])
+        updated_student = student.update!(student_params)
+        render json: student, status: :accepted
+    end
+    
+    def destroy
+        student = student.find(params[:id])
+        student.destroy
+        render json: student
+    end
+    
+    private
+    
+    def student_params
+        params.permit(:name, :address, :email, :birthday, :school_id, :auth_level)
+    end
+
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,36 @@
 class TeachersController < ApplicationController
+
+    def index
+        teachers = teacher.all
+        render json: teachers, status: :ok
+    end
+    
+    def show
+        teacher = teacher.find(params[:id])
+        render json: teacher, status: :ok
+    end
+    
+    def create
+        teacher = teacher.create!(teacher_params)
+        render json: teacher, status: :created
+    end
+    
+    def update
+        teacher = teacher.find(params[:id])
+        updated_teacher = teacher.update!(teacher_params)
+        render json: teacher, status: :accepted
+    end
+    
+    def destroy
+        teacher = teacher.find(params[:id])
+        teacher.destroy
+        render json: teacher
+    end
+    
+    private
+    
+    def teacher_params
+        params.permit(:name, :address, :subject, :email, :birthday, :school_id, :auth_level)
+    end
+    
 end
