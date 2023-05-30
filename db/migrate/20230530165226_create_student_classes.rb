f5183142ea21ae512b7106a7b78c1f00fe3493e5
istafamarshall <istafa.a.marshall@gmail.com>
2023-05-30 12:52:37 -0400

    create student_class model

---
+++ db/migrate/20230530165226_create_student_classes.rb
@@ -0,0 +1,11 @@
+class CreateStudentClasses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :student_classes do |t|
+      t.string :time
+      t.string :teacher_id
+      t.string :student_id
+
+      t.timestamps
+    end
+  end
+end
