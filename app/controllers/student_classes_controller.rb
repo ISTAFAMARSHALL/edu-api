18b7f771418b62298232184da00cc85d90876b31
istafamarshall <istafa.a.marshall@gmail.com>
2023-05-30 12:39:06 -0400

    add full CRUD to student_classess controller

--- app/controllers/student_classes_controller.rb
+++ app/controllers/student_classes_controller.rb
@@ -1,2 +1,36 @@
 class StudentClassesController < ApplicationController
+
+    def index
+        student_classes = student_class.all
+        render json: student_classes, status: :ok
+    end
+    
+    def show
+        student_class = student_class.find(params[:id])
+        render json: student_class, status: :ok
+    end
+    
+    def create
+        student_class = student_class.create!(class_params)
+        render json: student_class, status: :created
+    end
+    
+    def update
+        student_class = student_class.find(params[:id])
+        updated_student_class = student_class.update!(class_params)
+        render json: student_class, status: :accepted
+    end
+    
+    def destroy
+        student_class = student_class.find(params[:id])
+        student_class.destroy
+        render json: student_class
+    end
+    
+    private
+    
+    def class_params
+        params.permit(:name, :student_id, :teacher_id)
+    end
+
 end
