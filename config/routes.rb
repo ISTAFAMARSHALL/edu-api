f44770228061daa321d416eaf6fad115848ff418
istafamarshall <istafa.a.marshall@gmail.com>
2023-05-30 12:49:03 -0400

    create school model

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users
+  resources :schools
   resources :students
   resources :teachers
   resources :student_classes
