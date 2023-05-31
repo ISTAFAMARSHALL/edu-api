82b2d36a5c446fd399d3d7cdb57ca14cc32e39db
istafamarshall <istafa.a.marshall@gmail.com>
2023-05-31 13:48:45 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,13 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 10.times do |index|
-    School.create!(name: Faker::School.unique.name, address: Faker::School.unique.address)
+    School.create!(name: Faker::Educator.unique.primary_school, address: Faker::Address.unique.full_address)
+end
+
+100.times do |index|
+    Teacher.create!(name: Faker::Name.name, address: Faker::Address.unique.full_address, subject: Faker::Educator.subject, email: Faker::Internet.email, birthday: Faker::Date.birthday, school_id: rand(1..10), auth_level: "teacher" )
+end
+
+1000.times do |index|
+    Student.create!(name: Faker::Name.name, address: Faker::Address.unique.full_address, email: Faker::Internet.email, birthday: Faker::Date.birthday, school_id: rand(1..10), auth_level: "student" )
 end
