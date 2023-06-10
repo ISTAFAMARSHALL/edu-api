81f4015e73b09f18bb014e21067307b1ed65a7e3
istafamarshall <istafa.a.marshall@gmail.com>
2023-06-10 14:16:54 -0400

    add full CRUD to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+
+    # skip_before_action :authorize, only: [:create]
+    
+    def index
+        users = User.all 
+        render json: users, status: :ok
+    end
+    
+    def show
+        user = User.find(params[:id])
+        render json: user, status: :ok
+    end
+    
+    def create
+        user = User.create!(user_params).authenticate(params[:password])
+        render json: user, status: :created
+    end
+
+    def update
+        user = User.find(params[:id])
+        updated_user = user.update!(user_params)
+        render json: updated_user, status: :accepted
+    end
+    
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        head :no_content
+    end
+    
+    private
+    
+    def user_params
+        params.permit(:email , :password_digest , :password_confirmation)
+    end
+
 end
