42681ede2125ec1e03837b8e494a1bb24450cef6
istafamarshall <istafa.a.marshall@gmail.com>
2023-06-10 13:53:29 -0400

    create sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
+
+    include ActionController::Cookies
+    
+    # before_action :authorize
     
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
     rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
@@ -11,6 +15,11 @@ class ApplicationController < ActionController::API
 
     def record_invalid(invalid)
         render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+    def authorize
+        @current_user = User.find_by(id: session[:user_id])
+        render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
     end
 
 end
