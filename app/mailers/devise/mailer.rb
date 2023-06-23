a5d3c9e831984d3b883727f1899fdeac447e9df9
istafamarshall <istafa.a.marshall@gmail.com>
2023-06-22 23:36:52 -0400

    create mailer.rb

---
+++ app/mailers/devise/mailer.rb
@@ -0,0 +1,5 @@
+class MyDeviseMailer < Devise::Mailer   
+    default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
+  
+    # Add your custom methods, overrides, etc.
+  end
