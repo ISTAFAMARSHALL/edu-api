99f2ba167a7819430a4c99d3f5e45b751e65edec
istafamarshall <istafa.a.marshall@gmail.com>
2023-06-23 09:47:38 -0400

    remove mailers folder

--- app/mailers/devise/mailer.rb
+++
@@ -1,7 +0,0 @@
-require 'devise'
-
-class Mailer < Devise::Mailer  
-    default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
-  
-    # Add your custom methods, overrides, etc.
-  end
